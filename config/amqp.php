4dc8948ec0f0165e21bfbf90db65cadb9a43c331
Nawwar Elnarsh <sirnarsh@gmail.com>
2019-05-15 03:08:42 +0200

    Init basic AMQP & RabbitMQ Management API classes

--- config/amqp.php
+++ config/amqp.php
@@ -2,10 +2,10 @@
 
 return [
     // Host info
-    'host' => env('AMQP_HOST'),
-    'user' => env('AMQP_USER'),
-    'pass' => env('AMQP_PASS'),
-    'vhost' => env('AMQP_VHOST'),
+    'host' => env('AMQP_HOST', 'localhost'),
+    'user' => env('AMQP_USER', 'guest'),
+    'pass' => env('AMQP_PASS', 'guest'),
+    'vhost' => env('AMQP_VHOST', ''),
 
     // Queue name to bind to all exchanges in order to let rabbitrq gets all messages
     'queue_name' => env('rabbitrq.main'),
